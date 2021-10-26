80eb34c459eeb5fede9430a0ca47e6fe01f28658
skarambettu <47953540+skarambettu@users.noreply.github.com>
2021-10-26 12:14:58 -0700

    Update Profile.java

--- src/main/java/com/playstation/housie/profile/Profile.java
+++ src/main/java/com/playstation/housie/profile/Profile.java
@@ -12,42 +12,43 @@ import com.playstation.housie.ticket.HousieTicket;
 
 @Entity
 public class Profile {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	/**
-	 * Profile Id
-	 */
-	private long id;
-	
-	private String name;
-	
-	@OneToMany
-	private List<HousieTicket> tickets;
-	/**
-	 * @return the id
-	 */
-	public long getId() {
-		return id;
-	}
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  /**
+   * Profile Id
+   */
+  private long id;
 
-	/**
-	 * @param id the id to set
-	 */
-	public void setId(long id) {
-		this.id = id;
-	}
-	
-	/**
-     * @return the name
-     */
-    public String getName() {
-        return name;
-    }
+  private String name;
 
-    /**
-     * @param name the name to set
-     */
-    public void setName(String name) {
-        this.name = name;
-    }
+  @OneToMany
+  private List<HousieTicket> tickets;
+
+  /**
+   * @return the id
+   */
+  public long getId() {
+    return id;
+  }
+
+  /**
+   * @param id the id to set
+   */
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  /**
+   * @return the name
+   */
+  public String getName() {
+    return name;
+  }
+
+  /**
+   * @param name the name to set
+   */
+  public void setName(String name) {
+    this.name = name;
+  }
 }
